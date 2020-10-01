f27e3c8c7aaccf9f052c2e4f5a23189863a6d1ed
Ivan Gawr <ivangawr1993>
2020-10-01 19:57:29 +0300

    fix: task not completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n){
+    let min = n < 0 ? n * -1 : n
+    let arr = String(min).split('').reverse();
+      for(let i=0; i<=arr.length; i++){
+         if(arr[0] == 0){
+           arr.shift()
+           return arr.join('');
+         }
+        return arr.join('');
+     }
+   };
